a17bccbb9ec96cf136c367d8076c9f799683264e
reed-overflow <xbc@live.jp>
2022-10-14 16:31:07 +0800

    请求异步

--- src/main/java/com/github/reedoverflow/stage1streader/ui/panel/ForumListPanel.java
+++ src/main/java/com/github/reedoverflow/stage1streader/ui/panel/ForumListPanel.java
@@ -1,6 +1,7 @@
 package com.github.reedoverflow.stage1streader.ui.panel;
 
 import com.github.reedoverflow.stage1streader.domain.Forum;
+import com.github.reedoverflow.stage1streader.domain.Thread;
 import com.github.reedoverflow.stage1streader.service.DiscuzService;
 import com.github.reedoverflow.stage1streader.ui.ThreadListUI;
 import com.intellij.icons.AllIcons;
@@ -12,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.tree.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,81 +28,94 @@ public class ForumListPanel extends JPanel {
 
     public ForumListPanel() {
         super();
-        tree.setPaintBusy(true);
         createForumTree();
-
-        // tree渲染
-        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-        tree.setCellRenderer(new ColoredTreeCellRenderer() {
-            @Override
-            public void customizeCellRenderer(@NotNull JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-                Object userObject = ((DefaultMutableTreeNode) value).getUserObject();
-                if (userObject instanceof Forum) {
-                    if(((Forum) userObject).getSublist() == null || ((Forum) userObject).getSublist().size() == 0) {
-                        // 子节点
-                        setIcon(AllIcons.FileTypes.Text);
-                    }else {
-                        // 根节点
-                        setIcon(AllIcons.Nodes.Folder);
-                    }
-                    append(((Forum) userObject).getName());
-                } else {
-                    setIcon(null);
-                    append(value.toString());
-                }
-            }
-        });
-        // 节点监听
-        MouseListener ml = new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                int selRow = tree.getRowForLocation(e.getX(), e.getY());
-                TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
-                if(selRow != -1) {
-                    if(e.getClickCount() == 1) {
-                    }
-                    else if(e.getClickCount() == 2) {
-                        // 双击
-                        DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-                        if (node == null) {
-                            return;
-                        }
-                        Object nodeInfo = node.getUserObject();
-                        Forum forum = (Forum) nodeInfo;
-//                        Messages.showMessageDialog(forum.getDescription(), forum.getName(), Messages.getInformationIcon());
-                        String forumId = forum.getFid();
-                        if(StringUtil.isNotEmpty(forumId)) {
-                            ThreadListUI threadListUI = ThreadListUI.getInstance();
-                            threadListUI.getThreadByForumId(Integer.parseInt(forumId));
-                        }
-
-                    }
-                }
-            }
-        };
-        tree.addMouseListener(ml);
-
         this.add(tree);
-
     }
 
     /**
      * treeNode 节点设置
      */
     public void createForumTree() {
+        tree.setPaintBusy(true);
+
         top = new DefaultMutableTreeNode("Forum list");
-        DiscuzService discuzService = new DiscuzService();
-        List<Forum> forumList = discuzService.getForumList();
-
-        DefaultMutableTreeNode node = null;
-        for (Forum forum: forumList
-             ) {
-            node = new DefaultMutableTreeNode(forum);
-            setUpChildren(node, forum);
-            top.add(node);
-        }
-
         tree = new Tree(top);
-        tree.setPaintBusy(false);
+
+        SwingWorker forumWorker = new SwingWorker() {
+            List<Forum> forumList = new ArrayList<>();
+            @Override
+            protected Object doInBackground() throws Exception {
+                // 获取列表
+                DiscuzService discuzService = new DiscuzService();
+                forumList = discuzService.getForumList();
+                return forumList;
+            }
+
+            @Override
+            protected void done() {
+                super.done();
+                // 封装渲染
+                DefaultMutableTreeNode node = null;
+                for (Forum forum: forumList
+                ) {
+                    node = new DefaultMutableTreeNode(forum);
+                    setUpChildren(node, forum);
+                    top.add(node);
+                }
+                tree.setPaintBusy(false);
+
+                // tree渲染
+                tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+                tree.setCellRenderer(new ColoredTreeCellRenderer() {
+                    @Override
+                    public void customizeCellRenderer(@NotNull JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+                        Object userObject = ((DefaultMutableTreeNode) value).getUserObject();
+                        if (userObject instanceof Forum) {
+                            if(((Forum) userObject).getSublist() == null || ((Forum) userObject).getSublist().size() == 0) {
+                                // 子节点
+                                setIcon(AllIcons.FileTypes.Text);
+                            }else {
+                                // 根节点
+                                setIcon(AllIcons.Nodes.Folder);
+                            }
+                            append(((Forum) userObject).getName());
+                        } else {
+                            setIcon(null);
+                            append(value.toString());
+                        }
+                    }
+                });
+                // 节点监听
+                MouseListener ml = new MouseAdapter() {
+                    public void mouseClicked(MouseEvent e) {
+                        int selRow = tree.getRowForLocation(e.getX(), e.getY());
+                        TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
+                        if(selRow != -1) {
+                            if(e.getClickCount() == 1) {
+                            }
+                            else if(e.getClickCount() == 2) {
+                                // 双击
+                                DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+                                if (node == null) {
+                                    return;
+                                }
+                                Object nodeInfo = node.getUserObject();
+                                Forum forum = (Forum) nodeInfo;
+//                        Messages.showMessageDialog(forum.getDescription(), forum.getName(), Messages.getInformationIcon());
+                                String forumId = forum.getFid();
+                                if(StringUtil.isNotEmpty(forumId)) {
+                                    ThreadListUI threadListUI = ThreadListUI.getInstance();
+                                    threadListUI.getThreadByForumId(Integer.parseInt(forumId));
+                                }
+
+                            }
+                        }
+                    }
+                };
+                tree.addMouseListener(ml);
+            }
+        };
+        forumWorker.execute();
     }
 
     /**
